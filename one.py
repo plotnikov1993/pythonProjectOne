69e598636c6e03e0265c527a5141ecfe27c7e4d6
Андрей Плотников <plotnikov_andrew@icloud.com>
2022-10-06 20:27:56 +0300

    one

---
+++ one.py
@@ -0,0 +1,56 @@
+# Zadacha 1
+# num = int(input('Введите порядковый номер дня недели: '))
+#
+# if num == 6 or num == 7:
+#     print('данный день выходной')
+# elif num >=1 and num <= 5:
+#     print('данный день будний')
+# else:
+#     print('вводные данные не верны')
+import math
+
+# Zadacha 2
+# for x in range(2):
+#     for y in range(2):
+#         for z in range(2):
+#             print((x, y, z), not (x or y or z) == (not x and not y and not z))
+
+# Zadacha 3
+# x = int (input( 'введите координаты точки на оси x: '))
+# y = int (input( 'введите координаты точки на оси y: '))
+# if x > 0 and y > 0:
+#     print('точка находится в первой четверти')
+# elif x < 0 and y > 0:
+#     print('точка находится во второй четверти')
+# elif x < 0 and y < 0:
+#     print('точка находится в третей четверти')
+# elif x > 0 and y < 0:
+#     print('точка находится в четвертой четверти')
+# else: print('неверные координаты')
+
+# Zadacha 4
+# zone = int (input( 'введите номер четверти: '))
+#
+# if zone == 1:
+#      print('x > 0 , y > 0')
+# elif zone == 2:
+#      print('x < 0 , y > 0')
+# elif zone == 3:
+#      print('x < 0 , y < 0')
+# elif zone == 4:
+#      print('x > 0 , y < 0')
+# else: print('неверные координаты')
+
+
+
+# Zadacha 5
+
+# x1 = int (input( 'введите координаты точки 1 на оси x: '))
+# y1 = int (input( 'введите координаты точки 1 на оси y: '))
+# x2 = int (input( 'введите координаты точки 2 на оси x: '))
+# y2 = int (input( 'введите координаты точки 2 на оси y: '))
+# dist = math.sqrt((x2 - x1)**2+(y2-y1)**2)
+# print(round(dist,1))
+
+# Zadacha 6
+
---
+++ two.py
@@ -0,0 +1,36 @@
+# Zadacha 1
+# max = int(input('Введите число: '))
+# n = 1
+# while n <= max:
+#     print(n, ((3 * n) + 1))
+#     n += 1
+
+# Zadacha 2
+# N = int(input('Введите число: '))
+# sum = 1
+# i = 1
+# while i <= N:
+#     print(i, (sum*i))
+#     sum = sum * i
+#     i += 1
+
+# Zadacha 3
+# N = int(input('Введите число: '))
+# sum = 0
+# i = 1
+# while i <= N:
+#     form = (1 + (1 / i)) ** i
+#     i += 1
+#     sum += form
+# print('сумма', round(sum, 2))
+
+# Zadacha 5
+# import datetime
+# def fun(some_list):
+#     for i in range(0, len(some_list)-1):
+#         print(datetime.datetime.now().microsecond)
+#         j = datetime.datetime.now().microsecond % len(some_list)-1
+#         some_list[i], some_list[j] = some_list[j], some_list[i]
+#         return some_list
+# print(fun([1,2,3,4,5,6,7]))
+
